e325f499b218f9ea4cf36ce0c023882a1c3c7b7a
João Martinho <jmartinhoramos@gmail.com>
2023-06-18 11:56:24 +0100

    Register different types of users

--- BusinessLogic/Entities/RegistrationRequest.cs
+++ BusinessLogic/Entities/RegistrationRequest.cs
@@ -6,6 +6,7 @@
         public string Username { get; set; }
         public string Password { get; set; }
         public string Name { get; set; }
+        public bool Enable { get; set; }
         public string Role { get; set; } = "User";
     }
 }
